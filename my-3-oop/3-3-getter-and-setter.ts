8c91e0783761fc642bd56cb2f6ee81ea72049344
seongjun_kim <lewisel@naver.com>
2022-03-30 23:09:41 +0900

    (After) getter와 setter

--- my-3-oop/3-3-getter-and-setter.ts
+++ my-3-oop/3-3-getter-and-setter.ts
@@ -1,17 +1,28 @@
 {
   class User {
-    firstName: string;
-    lastName: string;
-    fullName: string;
-    constructor(firstName: string, lastName: string) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.fullName = `${lastName} ${firstName}`;
+    get fullName(): string {
+      return `${this.lastName} ${this.firstName}`;
     }
+    set fullName(name: string) {
+      const words = name?.split(" ");
+      if (words.length !== 2) {
+        throw new Error(
+          "New name should have only one blank(' ') between two words!"
+        );
+      }
+      this.firstName = words[0];
+      this.lastName = words[1];
+    }
+    // constructor 매개변수 정의부에서 private/public/protected와 함께 정의한 변수는 자동적으로 멤버 변수로 활용 가능하다!
+    // constructor(private firstName: string, private lastName: string) {}
+    constructor(private firstName: string, private lastName: string) {}
   }
 
-  const user = new User("Alien", "Ray");
-  console.log(user.fullName);
-  user.firstName = "Human";
-  console.log(user.fullName);
+  const user = new User("Sohyun", "Kim");
+  console.log(user.fullName); // Kim Sohyun
+  //   user.fullName("Human"); // 정의부에서는 함수형태로 작성하지만, 호출형이 아님! (not callable)
+  user.fullName = "Huisoo Jung";
+  console.log(user.fullName); // Jung Huisoo
+  //   user.fullName = "Human"; // Erro Case
+  //   console.log(user.fullName);
 }
